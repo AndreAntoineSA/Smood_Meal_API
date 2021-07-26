f1e5b9a7ffc82be3e0c61bb928aeea23d4ea23a4
andre <andreantoine0016@gmail.com>
2021-07-26 23:58:10 +0200

    corrections reducer

--- src/redux/reducers/mealReducer.js
+++ src/redux/reducers/mealReducer.js
@@ -1,39 +1,36 @@
 import { ActionTypes } from "../constants/action-types"
 
 const initialState = {
-    meals: { meals: [] }
+  meals: [],
 }
-export const mealReducer = (state = initialState, { type, payload }) =>
-{
-    switch (type)
-    {
-        case ActionTypes.SET_MEALS:
-            return { ...state, meals: payload }
-        default:
-            return state
-    }
+export const mealReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_MEALS:
+      return { ...state, ...payload }
+    default:
+      return state
+  }
 }
 
-export const selectedMealReducer = (state = { meals: [{}] }, { type, payload }) =>
-{
-    switch (type)
-    {
-        case ActionTypes.SELECTED_MEAL:
-            return { ...state, ...payload }
-        case ActionTypes.REMOVE_SELECTED_MEAL:
-            return { meals: [{}] }
-        default:
-            return state
-    }
+export const selectedMealReducer = (
+  state = { meals: [{}] },
+  { type, payload }
+) => {
+  switch (type) {
+    case ActionTypes.SELECTED_MEAL:
+      return { ...state, ...payload }
+    case ActionTypes.REMOVE_SELECTED_MEAL:
+      return { meals: [{}] }
+    default:
+      return state
+  }
 }
 
-export const contactReducer = (state = { }, { type, payload }) =>
-{
-    switch (type)
-    {
-        case ActionTypes.SET_CONTACT:
-            return { ...state, ...payload }
-        default:
-            return state
-    }
+export const contactReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_CONTACT:
+      return { ...state, ...payload }
+    default:
+      return state
+  }
 }
