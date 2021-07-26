eeb0e4a2e1407c4e88cd2f7c0867a289a43cd7a8
andre <andreantoine0016@gmail.com>
2021-07-26 17:25:55 +0200

    complete navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,59 @@
-import React, { useState } from 'react'
-import { FaTimes, FaBars, FaLinkedin } from 'react-icons/fa'
-import { IconContext } from 'react-icons/lib'
-import { Nav, NavbarContainer, NavLogo, MenuIcon, NavMenu, NavHead, NavItem, NavLinks, LinkedIcon } from './Navbar.elements'
+import React, { useState } from "react"
+import { FaTimes, FaBars, FaLinkedin } from "react-icons/fa"
+import { IconContext } from "react-icons/lib"
+import {
+  Nav,
+  NavbarContainer,
+  NavbarRight,
+  NavLogo,
+  MenuIcon,
+  NavMenu,
+  NavHead,
+  NavItem,
+  NavLinks,
+  LinkedIcon,
+} from "./Navbar.elements"
 
-const Navbar = () =>
-{
-    const [click, setClick] = useState(false)
+const Navbar = () => {
+  const [click, setClick] = useState(false)
 
-    const handleClick = () =>
-    {
-        setClick(!click)
-    }
+  const handleClick = () => {
+    setClick(!click)
+  }
 
-    return (
-        <>
-            <IconContext.Provider value={{ color: '#fff' }}>
-                <Nav>
-                    <NavbarContainer>
-                        <NavLogo to="/" >
-                            Smood Meal API Explorer
-                        </NavLogo>
-                        {/* justify flex-end */}
-                        <LinkedIcon href="https://www.linkedin.com/in/andre16/" target="_blank" >
-                            <FaLinkedin />
-                        </LinkedIcon>
-                        <MenuIcon onClick={handleClick}>
-                            {click ? <FaTimes /> : <FaBars />}
-                        </MenuIcon>
-                        <NavMenu onClick={handleClick} click={click}>
-                            <NavHead>Smood</NavHead>
-                            <NavItem>
-                                <NavLinks to='/' >Meals list</NavLinks>
-                            </NavItem>
-                            <NavItem>
-                                <NavLinks to='/contact' >Contact</NavLinks>
-                            </NavItem>
-                        </NavMenu>
-                    </NavbarContainer>
-                </Nav>
-            </IconContext.Provider>
-        </>
-
-    )
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              {process.env.REACT_APP_NAME} Meal API Explorer
+            </NavLogo>
+            <NavbarRight>
+              <LinkedIcon
+                href="https://www.linkedin.com/in/andre16/"
+                target="_blank"
+              >
+                <FaLinkedin />
+              </LinkedIcon>
+              <MenuIcon onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+              </MenuIcon>
+            </NavbarRight>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavHead>{process.env.REACT_APP_NAME}</NavHead>
+              <NavItem>
+                <NavLinks to="/">Meals list</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/contact">Contact</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  )
 }
 
 export default Navbar
